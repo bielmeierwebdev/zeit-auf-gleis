a543fe70680fc211d39921be2c287759c0bbbae7
bielmeierwebdev <bielmeierwebdevelop@gmail.com>
2026-01-08 17:59:53 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppShell from "../src/components/layout/AppShell";
 import ProtectedRoute from "../src/components/layout/ProtectedRoute";
 import Login from "../src/pages/Login";
-import Calendar from "../src/pages/Calendar";
 import Overview from "../src/pages/Overview";
-import Settings from "../src/pages/Settings";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Overview />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Overview />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
