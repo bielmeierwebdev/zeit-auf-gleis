b68638130ee9885f234e71dec705780590f5e1bf
bielmeierwebdev <bielmeierwebdevelop@gmail.com>
2026-01-14 12:32:00 +0100

    update

--- src/components/currentPeriod/CurrentPeriod.jsx
+++ src/components/currentPeriod/CurrentPeriod.jsx
@@ -1,16 +1,16 @@
 import { Box, Paper, Typography } from "@mui/material";
 import { MONTHS } from "../calendar/monthList";
+import { coworkerColors } from "../calendar/coWorkerColors";
 
 function CurrentPeriod({ monthStatusByWorker, hasMonthSheets, month, year }) {
-  
-    return (
+  return (
     <Paper sx={{ p: 3 }}>
       <Typography variant="subtitle2" color="text.secondary">
         Aktueller Zeitraum
       </Typography>
 
       <Typography variant="h6" sx={{ mt: 0.5 }}>
-        📅{" "}{MONTHS[month]}{" "}{year}
+        📅 {MONTHS[month]} {year}
       </Typography>
 
       <Typography
@@ -22,10 +22,25 @@ function CurrentPeriod({ monthStatusByWorker, hasMonthSheets, month, year }) {
           : "Für diesen Monat fehlen noch Einträge."}
       </Typography>
 
+      {console.log(coworkerColors)}
+
       <Box mt={2} display="flex" flexDirection="column" gap={0.5}>
         {monthStatusByWorker.map((w) => (
           <Box key={w.name} display="flex" justifyContent="space-between">
-            <Typography>{w.name}</Typography>
+            {/* Name + Dot */}
+            <Box display="flex" alignItems="center" gap={1}>
+              <Box
+                sx={{
+                  width: 8,
+                  height: 8,
+                  borderRadius: "50%",
+                  bgcolor: coworkerColors[w.name],
+                }}
+              />
+              <Typography>{w.name}</Typography>
+            </Box>
+
+            {/* Status */}
             <Typography
               color={w.hasSheet ? "success.main" : "error.main"}
               fontWeight={500}
